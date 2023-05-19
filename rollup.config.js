f09ea64ef76122addd10738d40a6a8e8510e1b1b
johnhom1024 <924358746@qq.com>
2023-05-19 23:37:52 +0800

    feat: 新增导出esm模块

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,8 @@
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import { defineConfig } from 'rollup'
+import { defineConfig } from 'rollup';
+import commonjs from '@rollup/plugin-commonjs';
 import path from 'path';
-
-import pkg from './package.json';
 
 export default defineConfig({
   input: path.resolve(__dirname, 'src/index.ts'),
@@ -11,19 +10,23 @@ export default defineConfig({
   output: [
     // commonJS
     {
-      file: pkg.main,
-      format: 'cjs'
-    }
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'es',
+    },
   ],
   plugins: [
     // 解析第三方依赖
     resolve(),
+    // commonjs一般与@rollup/plugin-node-resolve配合使用
+    commonjs(),
     // rollup 编译typescript
     typescript({
-      tsconfig: path.resolve(__dirname, 'tsconfig.json')
-    })
+      tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+    }),
   ],
-  external: [
-    /node_modules/
-  ]
-})
+  external: ['postcss'],
+});
