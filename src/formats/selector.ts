efbf77f91e8e329181d68b022f09cce2cac4118a
anthony <924358746@qq.com>
2023-05-23 19:40:21 +0800

    feat: 新增babel处理class中的表达式

--- src/formats/selector.ts
+++ src/formats/selector.ts
@@ -3,7 +3,21 @@ import type { ClassName, Root } from 'postcss-selector-parser';
 import escapeStringRegexp from 'escape-string-regexp';
 import { parse as styleParser } from 'postcss';
 import type { Rule } from 'postcss';
+
+import generate from '@babel/generator';
+import { parseExpression } from '@babel/parser';
+import traverse from '@babel/traverse';
+
+
 import { SimpleMappingChars2String } from '../lib/dict';
+import {
+  tagWithEitherClassAndHoverClassRegexp,
+  vueTemplateClassRegexp,
+  variableMatch,
+  variableRegExp,
+} from '../lib/reg';
+
+
 abstract class SelectorTransformer {
   mappingChars2String: Record<string, string>;
   constructor({
@@ -44,7 +58,7 @@ export class StyleSelectorTransformer extends SelectorTransformer {
   }: {
     customMappingChars2String: Record<string, string>;
   }) {
-    super({customMappingChars2String});
+    super({ customMappingChars2String });
   }
 
   selectorHandler(selector: string) {
@@ -58,18 +72,17 @@ export class StyleSelectorTransformer extends SelectorTransformer {
         if (classNode.value.length === 0) {
           classNode.remove();
         }
-      })
+      });
     }).processSync(selector);
 
     return result;
   }
 
-  styleHandler(rawSource: string) {
-
-    const root = styleParser(rawSource)
+  styleHandler(rawSource: string): string {
+    const root = styleParser(rawSource);
     root.walkRules((rule: Rule) => {
       rule.selector = this.selectorHandler(rule.selector);
-    })
+    });
 
     return root.toString();
   }
@@ -78,8 +91,80 @@ export class StyleSelectorTransformer extends SelectorTransformer {
    * @description: 处理html代码中的class
    * @param {string} rawSource
    * @return {*}
-   */  
-  htmlHandler(rawSource: string) {
+   */
+  templateHandler(rawSource: string) {
+    // 这里匹配开头的标签，例如：<view class="content">
+    rawSource.replace(tagWithEitherClassAndHoverClassRegexp, (m0) => {
+      return m0.replace(vueTemplateClassRegexp, (match, className) => {
+        // match 匹配的结构为 class="font-bold"
+        // className的结构为 font-bold flex-[0_0_300rpx] 等
+        return match.replace(className, this.templateReplacer(className));
+      });
+    });
+  }
 
+  templateReplacer(className: string): string {
+    // 检查class属性是否传入了变量
+    let match = variableMatch(className);
+    const sources = [];
+
+    while (match !== null) {
+      sources.push({
+        start: match.index,
+        end: variableRegExp.lastIndex,
+        raw: match[1],
+      });
+
+      match = variableMatch(className);
+    }
+
+    if (sources.length) {
+      const resultArray = [];
+
+      let p = 0;
+      for (let i = 0; i < sources.length; i++) {
+        const m = sources[i];
+        resultArray.push(this.transform(className.slice(p, m.start)));
+
+        p = m.start;
+
+        // 匹配后值
+        if (m.raw.trim().length) {
+          const code = this.generateCode(m.raw);
+          m.raw = `{{${code}}}`;
+        } else {
+          m.raw = '';
+        }
+
+        resultArray.push(m.raw);
+
+        p = m.end;
+      }
+
+      return resultArray.filter((x) => x).join('');
+    } else {
+      return this.transform(origin);
+    }
+  }
+
+  generateCode(code:string) {
+    const ast = parseExpression(code);
+  
+    traverse(ast, {
+      StringLiteral: (path) =>  {
+        path.node.value = this.transform(path.node.value);
+      },
+      noScope: true,
+    })
+
+    const { code: newCode } = generate(ast, {
+      compact: true,
+      minified: true,
+      jsescOption: {
+        quotes: 'single'
+      }
+    });
+
+    return newCode;
   }
 }
--- src/lib/reg.ts
+++ src/lib/reg.ts
@@ -1,4 +1,4 @@
-const vueTemplateClassRegexp =
+export const vueTemplateClassRegexp =
   /(?:(?:hover-)?class)=(?:["']\W+\s*(?:\w+)\()?["']([^"]+)['"]/gs;
 
 // 匹配 <tag class=""></tag>
@@ -6,6 +6,12 @@ const vueTemplateClassRegexp =
 // 匹配 <tag hover-class=""></tag>
 // 匹配 <tag :hover-class="[{}]"></tag>
 
-// 然后拿到
-const tagWithEitherClassAndHoverClassRegexp =
+export const tagWithEitherClassAndHoverClassRegexp =
   /<(?:[a-z][-a-z]*[a-z]*)\s+[^>]*?(?:(?:hover-)?class="(?:[^"]*)")[^>]*?\/?>/g;
+
+  // 匹配变量的正则
+export const variableRegExp = /{{(.*?)}}/gs;
+
+export function variableMatch(original: string) {
+  return variableRegExp.exec(original);
+}
