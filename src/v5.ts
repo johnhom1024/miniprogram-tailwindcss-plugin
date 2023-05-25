e9d06c03be4ae7a10626aa0c2738d9f1204add17
johnhom1024 <924358746@qq.com>
2023-05-25 22:09:49 +0800

    feat: 支持webpack5版本

--- src/v5.ts
+++ src/v5.ts
@@ -1,18 +1,25 @@
 import { Compilation, Compiler, WebpackPluginInstance } from 'webpack';
+import { PluginOptions } from './types';
+import { Source } from 'webpack-sources';
+import { WechatSelectorTransformer } from './formats/selector';
+import { SimpleMappingChars2String } from './lib/dict';
 
 export class UniappTailwindcssWebpackPlugin implements WebpackPluginInstance {
   options = {};
-
-  constructor(options: any) {
+  selectorTransformer;
+  constructor(options = {} as PluginOptions) {
     this.options = options;
+    const { customMappingChars2String = SimpleMappingChars2String } = options;
+    this.selectorTransformer = new WechatSelectorTransformer({
+      customMappingChars2String,
+    });
   }
 
   apply(compiler: Compiler) {
-
     const { webpack } = compiler;
     const { Compilation } = webpack;
     const { RawSource } = webpack.sources;
-    
+
     const pluginName = UniappTailwindcssWebpackPlugin.name;
     compiler.hooks.compilation.tap(pluginName, (compilation: Compilation) => {
       compilation.hooks.processAssets.tap(
@@ -21,7 +28,19 @@ export class UniappTailwindcssWebpackPlugin implements WebpackPluginInstance {
           stage: Compilation.PROCESS_ASSETS_STAGE_SUMMARIZE,
         },
         (assets) => {
-          console.log('----------johnhomLogDebug hello world', )
+          const entries = Object.entries(assets);
+
+          for (const [filename, originalSource] of entries) {
+            this.selectorTransformer.traverse(
+              filename,
+              originalSource as Source,
+              (newSource: string) => {
+                const newAssets = new RawSource(newSource);
+
+                compilation.updateAsset(filename, newAssets);
+              }
+            );
+          }
         }
       );
     });
