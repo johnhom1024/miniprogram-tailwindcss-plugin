f206ac88471744bfe61d2710c4d11250af9a9bbe
anthony <924358746@qq.com>
2024-02-22 18:40:10 +0800

    feat: 去掉无用的测试用例

--- test/selector.test.ts
+++ test/selector.test.ts
@@ -1,17 +1,11 @@
 import { describe, expect, it } from 'vitest';
-import { WechatSelectorTransformer } from '../src/formats/selector';
+import { WechatSelectorTransformer } from '@/formats/selector';
 import { SimpleMappingChars2String } from '@/lib/dict';
-import { getTwCss } from '@test/helpers/getTwCss';
 
 describe('测试selector转换', () => {
   const selectorTransformer = new WechatSelectorTransformer({
     customMappingChars2String: SimpleMappingChars2String,
   });
-
-  it('测试getTwCss', async () => {
-    const res = await getTwCss('<view class="h-10 w-10 h-[10rpx]"></view>')
-    expect(res.css).toBe('h-10 w-10 bg-rgba25525425305')
-  })
 
   it('测试wxml转换"["或者"]"', () => {
     const wxmlString = `<div class="w-[20rpx]"></div>`;
